7be27243e2dd367b109e88a333f8ffadd59b5e59
noahpace124 <noahpace124@gmail.com>
2025-05-13 12:38:41 -0600

    finished prep1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        int percentage = int.Parse(Console.ReadLine());
+        Console.WriteLine("");
+        Console.Write("Your grade is: ");
+        if (percentage >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (percentage >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (percentage >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (percentage >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed.");
+        }
+
     }
 }
