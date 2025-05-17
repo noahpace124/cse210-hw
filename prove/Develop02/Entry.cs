a09c3f45413088f1f5bc0f5c49624f5f76cf1c04
noahpace124 <noahpace124@gmail.com>
2025-05-17 09:37:13 -0600

    Finished journal program.

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,14 @@
+public class Entry
+{
+    public string dateText;
+    public string promptText;
+    public string responseText;
+
+    public Entry(string dateText, string promptText, string responseText)
+    {
+        this.dateText = dateText;
+        this.promptText = promptText;
+        this.responseText = responseText;
+    }
+
+}
---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,105 @@
+using System.IO; 
+
+public class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+    private List<string> _prompts = new List<string>
+    {
+        "What was the best part of your day?",
+        "What are you grateful for today?",
+        "What did you learn today?",
+        "What did you accomplish today?",
+        "If I could do one thing differently today, what would it be?"
+    };
+
+    public void Run()
+    {
+        while (true)
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+
+            Console.Write("What would you like to do? ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Write();
+                    break;
+                case 2:
+                    Display();
+                    break;
+                case 3:
+                    Load();
+                    break;
+                case 4:
+                    Save();
+                    break;
+                case 5:
+                    return;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public void Write()
+    {
+        Random random = new Random();
+        int index = random.Next(_prompts.Count);
+        string prompt = _prompts[index];
+        Console.WriteLine(prompt);
+        Console.Write("> ");
+        string response = Console.ReadLine();
+        DateTime theCurrentTime = DateTime.Now;
+        string dateText = theCurrentTime.ToShortDateString();
+        Entry entry = new Entry(dateText, prompt, response);
+        _entries.Add(entry);
+    }
+
+    public void Display()
+    {
+        foreach (Entry entry in _entries)
+        {
+            Console.WriteLine($"Date: {entry.dateText} - Prompt: {entry.promptText}");
+            Console.WriteLine(entry.responseText);
+            Console.WriteLine();
+        }
+    }
+
+    public void Save()
+    {
+        Console.WriteLine("What is the filename?");
+        string filename = Console.ReadLine();
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry.dateText}|{entry.promptText}|{entry.responseText}");
+            }
+        }
+    }
+
+    public void Load()
+    {
+        Console.WriteLine("What is the filename?");
+        string filename = Console.ReadLine();
+        
+        string[] lines = System.IO.File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split('|');
+            string dateText = parts[0];
+            string promptText = parts[1];
+            string responseText = parts[2];
+            Entry entry = new Entry(dateText, promptText, responseText);
+            _entries.Add(entry);
+        }
+    }
+}
